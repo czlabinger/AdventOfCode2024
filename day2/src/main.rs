fb1d598e736fab5a8303d1a80025ad48f82c230a
czlabinger <czlabinger@student.tgm.ac.at>
2024-12-02 18:27:58 +0100

    Day2/2 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,61 +1,55 @@
 use std::fs;
 
-fn main() {
-    let content: String = fs::read_to_string("./input").unwrap();
-
-    let mut out: i32 = 0;
-
-    for line in content.lines() {
-    
-        let mut desc: i32 = 0;
-        let mut last = 0;
-        let mut valid = true;
-        
-        for e in line.split_whitespace() {
-            let curr: i32 = e.parse::<i32>().unwrap();
-      
-            println!("last: {}\ncurr: {}", last, curr);
-
-            if last == 0 {
-                last = curr.clone();
-                continue;
-            }
-
-            if last == curr {
-                valid = false;
-                break;
-            }
-
-            if last > curr && desc == 0 {
-                desc = 1;
-            } else if last < curr && desc == 0 {
-                desc = 2;
-            } else if last == curr && desc == 0 {
-                valid = false;
-                break;
-            }
-
-
-            if last > curr && desc == 2 {
-                valid = false;
-            }
-
-            if last < curr && desc == 1 {
-                valid = false;
-            }
-
-            if (last - curr).abs() > 3 && (last - curr).abs() > 0 {
-                valid = false;
-            }
-
-            last = curr.clone();
-
+fn is_safe(nums: &[i32]) -> bool {
+    let mut desc = 0;
+    for w in nums.windows(2) {
+        let diff = w[1] - w[0];
+        if diff.abs() > 3 {
+            return false;
         }
-        if valid {
-            println!("Line: {} is {}", line, valid);
-            out += 1;
+        
+        if diff == 0 {
+            return false;
+        }
+        
+        if desc == 0 {
+            desc = if diff > 0 { 1 } else { 2 };
+        } else if (diff > 0 && desc == 2) || (diff < 0 && desc == 1) {
+            return false;
         }
     }
-    println!("{}", out);
+    true
+}
 
+fn main() {
+    let content: String = fs::read_to_string("./input").unwrap();
+    let mut out: i32 = 0;
+    
+    for line in content.lines() {
+        let nums: Vec<i32> = line.split_whitespace()
+            .map(|e| e.parse::<i32>().unwrap())
+            .collect();
+        
+        // Check if original sequence is safe
+        if is_safe(&nums) {
+            out += 1;
+            continue;
+        }
+        
+        // Try removing each level
+        for i in 0..nums.len() {
+            let modified: Vec<i32> = nums.iter()
+                .enumerate()
+                .filter(|&(idx, _)| idx != i)
+                .map(|(_, &val)| val)
+                .collect();
+            
+            if is_safe(&modified) {
+                out += 1;
+                break;
+            }
+        }
+    }
+    
+    println!("{}", out);
 }
