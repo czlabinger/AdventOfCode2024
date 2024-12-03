400cb6228ff4fc15e201c0ecca83ca078665977c
czlabinger <czlabinger@student.tgm.ac.at>
2024-12-03 08:44:00 +0100

    Day3/1 done

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let content: String = fs::read_to_string("./input").unwrap();
+    let regex: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap(); 
+    let mut out: u32 = 0;
+
+    for line in content.lines() {
+        for cap in regex.captures_iter(line) {
+            out += cap[1].parse::<u32>().unwrap() * cap[2].parse::<u32>().unwrap();
+        }
+    }
+
+    println!("{}", out);
 }
